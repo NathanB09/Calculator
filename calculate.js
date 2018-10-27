827d604daa8b65394b147002be3e9d7d7ddfcb76
Nathan <NathanB09@users.noreply.github.com>
2018-10-27 15:50:00 +0100

    Included JQuery.

--- calculate.js
+++ calculate.js
@@ -1,24 +1,26 @@
-var output = document.getElementById("output-box")
+$(document).ready(function() {
+  var output = document.getElementById("output-box")
 
-document.addEventListener("click", function(e) {
-  //Ensures only a clicked button triggers any further event
-  if(e.target.nodeName === "BUTTON") {
-    //Sets a variable equal to the button clicked
-    var b = e.target.innerHTML
+  document.addEventListener("click", function(e) {
+    //Ensures only a clicked button triggers any further event
+    if(e.target.nodeName === "BUTTON") {
+      //Sets a variable equal to the button clicked
+      var b = e.target.innerHTML
 
-    if (b === "=") {
-      try {
-        output.innerHTML = eval(output.innerHTML)
-      } catch(err) {
+      if (b === "=") {
+        try {
+          output.innerHTML = eval(output.innerHTML)
+        } catch(err) {
+          output.innerHTML = ""
+          alert("Invalid entry.")
+        }
+      } else if (b === "AC") {
         output.innerHTML = ""
-        alert("Invalid entry.")
+      } else if (b === "C") {
+        output.innerHTML = output.innerHTML.slice(0, output.innerHTML.length - 1)
+      } else {
+        output.innerHTML += b
       }
-    } else if (b === "AC") {
-      output.innerHTML = ""
-    } else if (b === "C") {
-      output.innerHTML = output.innerHTML.slice(0, output.innerHTML.length - 1)
-    } else {
-      output.innerHTML += b
     }
-  }
+  })
 })
